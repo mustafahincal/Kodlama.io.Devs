3acaf3f14a385d60980a568bbddede56bb457a65
mustafahincal <mustafahncal@gmail.com>
2022-11-07 13:20:28 +0300

    add SubTechnology's controller and its methods

--- src/main/java/kodlama_io/kodlama_io_devs/api/controllers/ProgrammingLanguagesController.java
+++ src/main/java/kodlama_io/kodlama_io_devs/api/controllers/ProgrammingLanguagesController.java
@@ -1,10 +1,11 @@
 package kodlama_io.kodlama_io_devs.api.controllers;
 
 import kodlama_io.kodlama_io_devs.entities.ProgrammingLanguage;
+import kodlama_io.kodlama_io_devs.requests.ProgrammingLanguageAddRequest;
+import kodlama_io.kodlama_io_devs.requests.ProgrammingLanguageDeleteRequest;
+import kodlama_io.kodlama_io_devs.requests.ProgrammingLanguageUpdateRequest;
 import kodlama_io.kodlama_io_devs.services.abstracts.ProgrammingLanguageService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,19 @@ public class ProgrammingLanguagesController {
     @GetMapping("/getall")
     public List<ProgrammingLanguage> getAll(){
         return this.programmingLanguageService.getAll();
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody ProgrammingLanguageAddRequest programmingLanguageAddRequest){
+        this.programmingLanguageService.add(programmingLanguageAddRequest);
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody ProgrammingLanguageDeleteRequest programmingLanguageDeleteRequest){
+        this.programmingLanguageService.delete(programmingLanguageDeleteRequest);
+    }
+    @PostMapping("/update")
+    public void update(@RequestBody ProgrammingLanguageUpdateRequest programmingLanguageUpdateRequest){
+        this.programmingLanguageService.update(programmingLanguageUpdateRequest);
     }
 }
--- src/main/java/kodlama_io/kodlama_io_devs/api/controllers/SubTechnologiesController.java
+++ src/main/java/kodlama_io/kodlama_io_devs/api/controllers/SubTechnologiesController.java
@@ -3,10 +3,9 @@ package kodlama_io.kodlama_io_devs.api.controllers;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import kodlama_io.kodlama_io_devs.entities.SubTechnology;
+import kodlama_io.kodlama_io_devs.requests.*;
 import kodlama_io.kodlama_io_devs.services.abstracts.SubTechnologyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,19 @@ public class SubTechnologiesController {
     @GetMapping("/getall")
     public List<SubTechnology> getAll(){
         return this.subTechnologyService.getAll();
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody SubTechnologyAddRequest subTechnologyAddRequest){
+        this.subTechnologyService.add(subTechnologyAddRequest);
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody SubTechnologyDeleteRequest subTechnologyDeleteRequest){
+        this.subTechnologyService.delete(subTechnologyDeleteRequest);
+    }
+    @PostMapping("/update")
+    public void update(@RequestBody SubTechnologyUpdateRequest subTechnologyUpdateRequest){
+        this.subTechnologyService.update(subTechnologyUpdateRequest);
     }
 }
