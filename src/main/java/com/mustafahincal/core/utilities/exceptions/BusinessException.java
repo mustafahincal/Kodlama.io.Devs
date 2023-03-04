1ac4a8ec125fc0a6acc562427e0218080f901784
mustafahincal <mustafahncal@gmail.com>
2023-03-04 13:57:30 +0300

    add exception handler

---
+++ src/main/java/com/mustafahincal/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.mustafahincal.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
